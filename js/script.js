c06946e69aa600eaedc313bb5d55cd6d9614a022
Schlink <aanyehschlink@yahoo.com>
2021-05-31 13:28:08 +0000

    modified js and html files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Creating the accordion
 var acc = document.getElementsByClassName("accordion");
 var i = 0;
 
@@ -17,3 +18,16 @@ for(i = 0; i < acc.length; i++) {
         }
     })
 }
+//End of accordion
+
+//Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60b4cf026699c7280da9de0f/1f714haoe';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+//End of Tawk.to Script
